9a6c57cf092e98dcdfce74971f6c83beba9f4e98
Ye Lwin <yelwin2949@gmail.com>
2022-04-06 11:30:28 +0630

    clent setup

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Footer() {
+    return (
+        <footer>
+            <Container>
+                <a href="https://github.com/chiragdatwani/mern-e-commerce" target='blank'>
+                    <p>Chirag Datwani</p>
+                    <img src={process.env.PUBLIC_URL + '/icons/github-32.png'} alt="git-hub" />
+                </a>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer;
+
+const Container = styled.div`
+    & a{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-decoration: none;
+        color: black;
+        & img{
+            width: 25px;
+            margin-left: 10px;
+        }
+    }
+`
